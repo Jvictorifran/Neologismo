8442f8415421f031e6cd11c5036786191d6b777f
vhld7 <vitordauzacker007@gmail.com>
2026-01-20 19:41:36 -0400

    Sistema de usuario novo

--- backend/neologismo/admin.py
+++ backend/neologismo/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Neologismo
 # Register your models here.
+
+admin.site.register(Neologismo)
--- backend/neologismo/models.py
+++ backend/neologismo/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Neologismo (models.Model):
     #sobre o neologismo
-    termo = models.CharField(help_text='Nome do Neologismo', max_length=50, unique=True)
+    termo = models.CharField(help_text='Nome do Neologismo', max_length=50)
     significado = models.TextField(help_text='O que significa esse neologismo')
     exemplo_de_uso = models.TextField(help_text='Exemplo de uso do neologismo no dia á dia')
 
@@ -12,8 +13,8 @@ class Neologismo (models.Model):
     aprovado = models.BooleanField(default=False)
 
     #sobre o usuario/autor
-    data_criacao = models.DateTimeField(auto_now=True)
-    autor = models.CharField(max_length=100, blank=True, null=True, default='Anônimo')
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.CASCADE) # link para tabela dos usuarios
 
     def __str__(self):
         return self.termo
